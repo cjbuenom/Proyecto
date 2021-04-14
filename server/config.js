3d956a808aded601137030c7c2e510a97746ff57
Cristian Bueno <lab.computacion.vi@gmail.com>
2021-04-13 20:24:01 -0400

    initial commit

---
+++ server/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  port: process.env.PORT || 8080,
+  db: {
+    prod: process.env.DATABASE_URL || "mongodb+srv://UserDB:To7gWlphCNf1sk4J@mongodbcb.xoor2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    test: 'mongodb://localhost/stackoverflow-test',
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'development_secret',
+    expiry: '7d'
+  }
+};
